9cfc250cef9bf1ec1c23841f3cf84ded0af58579
Abdelrahman Elshafei <abdelrahmanelshafei@Abdelrahman-ElShafay.local>
2025-05-24 17:00:17 -0400

    Made ASCII Image More Clearer

--- ASCII-Camera.py
+++ ASCII-Camera.py
@@ -4,10 +4,12 @@ import numpy as np
 
 # Last bits of characters get the darkest coloured pixels while the first characters get the opposite.
 # Usage of tuples as its more memory effecient and faster in proformance
-ascii_chars = ('G', 'N', 'i', 'K', '0', 'O', 'd', '&', 'X', 'k', '?', '*', 'o', 'x', 'l', 'c', ';', ':', ',', "'", '.', ' ')
+ascii_chars = (' ', '.', "'", ',', ':', ';', 'c', 'l', 'x', 'o', '*', '?', 'k', 'X', '&', 'd', 'O', '0', 'K', 'i', 'N', 'G')
 
 # To not show actual live webcam footage
 SHOW_REAL_VIDEO = False
+
+COLOURIZED = False
 
 def rowToAscii(row):
     scale = len(ascii_chars)
@@ -33,12 +35,16 @@ while(cv2.waitKey(1) & 0xFF != ord('q')):
     if not ret:
         break
 
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Gray scale the actual colorized frames
+    if not COLOURIZED:
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # Gray scale the actual colorized frames
 
-    adjusted_frame = cv2.resize(gray_frame, (int(screen_height), int(screen_width))) # adjusted cv window to the terminal's dimensions
+        adjusted_frame = cv2.resize(gray_frame, (int(screen_height), int(screen_width))) # adjusted cv window to the terminal's dimensions
 
-    ascii_frame = pixelToAscii(adjusted_frame)
-    showAscii(ascii_frame)
+        ascii_frame = pixelToAscii(adjusted_frame)
+        showAscii(ascii_frame)
+
+    # else:
+        
 
     if SHOW_REAL_VIDEO:
         cv2.imshow('Grayscale Frame', adjusted_frame)
@@ -49,4 +55,3 @@ while(cv2.waitKey(1) & 0xFF != ord('q')):
 # Release capture data when everything is done
 capture.release()
 cv2.destroyAllWindows()
-
